32bf35c7cf726a2332a9030bfecacae6ee66d1d8
Moumouuu <robin.travail007@gmail.com>
2023-03-08 09:56:20 +0100

    ajout du début du dessin du labyrinthe

--- Pluviaux_Garnier_Jansen/app/src/main/java/com/example/pluviaux_garnier_jansen/ui/home/HomeFragment.java
+++ Pluviaux_Garnier_Jansen/app/src/main/java/com/example/pluviaux_garnier_jansen/ui/home/HomeFragment.java
@@ -1,20 +1,32 @@
 package com.example.pluviaux_garnier_jansen.ui.home;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pluviaux_garnier_jansen.R;
 import com.example.pluviaux_garnier_jansen.databinding.FragmentHomeBinding;
+import com.example.pluviaux_garnier_jansen.labyrinthe.ISalle;
+import com.example.pluviaux_garnier_jansen.labyrinthe.Labyrinthe;
+import com.example.pluviaux_garnier_jansen.labyrinthe.LabyrintheView;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private LabyrintheView LabyrintheView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +38,9 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
+
     }
 
     @Override
